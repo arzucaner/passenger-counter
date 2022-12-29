65bc2f6814ce96c7f4280ce946e8de7f07c6989b
Arzu Caner <arzuguneycaner@gmail.com>
2022-12-29 01:01:08 +0000

    Create passenger-counter

---
+++ script.js
@@ -0,0 +1,16 @@
+let count = 0
+let saveEL = document.getElementById("save-el")
+let countEL = document.getElementById("count-el")
+
+function increment() {
+    count += 1
+    countEL.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEL.textContenttSTR
+    countEL = 0
+}
+
+console.log("Let's count people on the train")
